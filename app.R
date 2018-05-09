00cc23530fb3e1c471bdae0726e3d49d1917ccbf
Peter M C Harrison <pmc.harrison@gmail.com>
2018-05-09 10:32:06 +0100

    Updating to latest psychTest version and producing some examples for Klaus

--- app.R
+++ app.R
@@ -6,11 +6,27 @@ options(shiny.error = browser)
 
 elts <- c(
   psychTest::one_button_page("Welcome!"),
+  psychTest::one_button_page("Here's a simple example of how you could give feedback to a participant."),
+  psychTest::NAFC_page(label = "favourite_colour",
+                       prompt = "What's your favourite colour?",
+                       choices = c("Yellow", "Red", "Green"),
+                       on_complete = function(state, answer, ...)
+                         psychTest::set_global(key = "fave", value = answer,
+                                               state = state)),
+  psychTest::reactive_page(function(state, ...) {
+    psychTest::one_button_page(
+      paste0("Your favourite colour is ",
+             psychTest::get_global("fave", state),
+             ".")
+    )
+  }),
+  psychTest::one_button_page("Now for the main test."),
   ca_bat(num_items = 5),
   psychTest::final_page("Thanks for taking part.")
 )
 
 psychTest::make_test(elts,
-                     "CA-BAT",
-                     options = psychTest::pt_options(admin_password = "conifer",
-                                                     demo = TRUE))
+                     opt = psychTest::pt_options(title = "CA-BAT",
+                                                 admin_password = "conifer",
+                                                 researcher_email = "p.m.c.harrison@qmul.ac.uk",
+                                                 demo = TRUE))
