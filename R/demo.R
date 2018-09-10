1e5237b8b7fdd999dde81a5f213a391087e42c04
Peter M C Harrison <pmc.harrison@gmail.com>
2018-09-10 10:15:10 +0100

    Defaulting to graph feedback page

--- R/demo.R
+++ R/demo.R
@@ -1,21 +1,15 @@
 #' @export
 demo_cabat <- function(num_items = 25L,
                        take_training = TRUE,
-                       feedback = cabat::cabat.feedback.simple_score(),
+                       feedback = cabat.feedback.graph(),
                        admin_password = "demo",
                        researcher_email = "p.m.c.harrison@qmul.ac.uk",
-                       languages = cabat_languages(),
+                       languages = "EN",
                        dict = cabat::cabat_dict) {
-  elts <- c(
-    cabat::cabat(num_items = num_items,
-                 take_training = take_training,
-                 feedback = cabat::cabat.feedback.simple_score(),
-                 dict = dict),
-    psychTestR::new_timeline(
-      psychTestR::final_page(psychTestR::i18n("you_may_close_browser")),
-      dict = dict
-    )
-  )
+  elts <- cabat::cabat(num_items = num_items,
+                       take_training = take_training,
+                       feedback = feedback,
+                       dict = dict)
 
   psychTestR::make_test(
     elts,
