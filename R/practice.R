cce054f809f42e4ad629bd5352ff32b349a2cdef
Peter M C Harrison <pmc.harrison@gmail.com>
2018-09-11 22:25:23 +0100

    Bugfix

--- R/practice.R
+++ R/practice.R
@@ -9,15 +9,15 @@ practice <- function(practice_items) {
     ),
     function(x) {
       list(
-        # psychTestR::audio_NAFC_page(
-        #   label = "practice_question",
-        #   prompt = shiny::HTML(psychTestR::i18n("ABAT_0009_I_0001_1",
-        #                                         sub = list(no_example = x$no))),
-        #   url = file.path(practice_items, paste0(x$id, ".mp3")),
-        #   choices = as.character(c(1, 2)),
-        #   labels = lapply(c("ABAT_0010_I_0001_1", "ABAT_0015_I_0001_1"), psychTestR::i18n),
-        #   save_answer = FALSE
-        # ),
+        psychTestR::audio_NAFC_page(
+          label = "practice_question",
+          prompt = shiny::HTML(psychTestR::i18n("ABAT_0009_I_0001_1",
+                                                sub = list(no_example = x$no))),
+          url = file.path(practice_items, paste0(x$id, ".mp3")),
+          choices = as.character(c(1, 2)),
+          labels = lapply(c("ABAT_0010_I_0001_1", "ABAT_0015_I_0001_1"), psychTestR::i18n),
+          save_answer = FALSE
+        ),
         psychTestR::reactive_page(function(answer, ...) {
           psychTestR::one_button_page(
             if (answer == x$answer)
