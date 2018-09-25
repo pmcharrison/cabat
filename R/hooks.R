796ef54f3717607206ca43261e36c66176f95756
Peter M C Harrison <pmc.harrison@gmail.com>
2018-09-25 21:11:12 +0100

    Adding package startup notification

---
+++ R/hooks.R
@@ -0,0 +1,8 @@
+.onLoad <- function(libname, pkgname) {
+  packageStartupMessage(
+    "Note: on September 25th 2018, two changes were made to ",
+    "default arguments for cabat(), demo_cabat(), and standalone_cabat():\n",
+    "- next_item.estimator now defaults to 'BM', not 'WL';\n",
+    "- fix_first_item now defaults to FALSE, not TRUE.\n"
+  )
+}
