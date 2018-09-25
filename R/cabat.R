d8019af11e7f30af470c310d61b5ae1fd28a1d6d
Peter M C Harrison <pmc.harrison@gmail.com>
2018-09-25 10:12:41 +0100

    Working on new documentation and changed test defaults

--- R/cabat.R
+++ R/cabat.R
@@ -1,3 +1,45 @@
+#' CABAT
+#'
+#' This function defines a CA-BAT module for incorporation into a
+#' psychTestR timeline.
+#' Use this function if you want to include the CA-BAT in a
+#' battery of other tests, or if you want to add custom psychTestR
+#' pages to your test timeline.
+#' For demoing the CA-BAT, consider using \link{\code{demo_cabat}}.
+#' For a standalone implementation of the CA-BAT,
+#' consider using \link{\code{standalone_cabat}}.
+#' @param num_items (Integer scalar) Number of items in the test.
+#' @param take_training (Logical scalar) Whether to include the training phase.
+#' @param label (Character scalar) Label to give the CA-BAT results in the output file.
+#' @param feedback (Function) Defines the feedback to give the participant
+#' at the end of the test.
+#' @param item_bank_audio (Character scalar) File path to the directory
+#' hosting the item bank audio (typically a publicly accessible web directory).
+#' @param practice_items (Character scalar) File path to the directory
+#' hosting the practice items audio (typically a publicly accessible web directory).
+#' @param next_item.criterion (Character scalar)
+#' Criterion for selecting successive items in the adaptive test.
+#' See the \code{criterion} argument in \link[catR]{nextItem} for possible values.
+#' \code{"bOpt"} corresponds to the setting used in the original CA-BAT paper.
+#' @param next_item.estimator (Character scalar)
+#' Ability estimation method used for selecting successive items in the adaptive test.
+#' See the \code{method} argument in \link[catR]{thetaEst} for possible values.
+#' \code{"BM"}, Bayes modal,
+#' corresponds to the setting used in the original CA-BAT paper.
+#' \code{"WL"}, weighted likelihood,
+#' corresponds to the default setting used in versions <= 0.3.0 of this package.
+#' @param next_item.prior_dist (Character scalar)
+#' The type of prior distribution to use when calculating ability estimates
+#' for item selection.
+#' Ignored if \code{next_item.estimator} is not a Bayesian method.
+#' Defaults to \code{"norm"} for a normal distribution.
+#' See the \code{priorDist} argument in \link[catR]{thetaEst} for possible values.
+#' @param next_item.prior_par
+#' Parameters for the normal
+#' @param final_ability.estimator
+#' @param constrain_answers
+#' @param fix_first_item
+#' @param dict
 #' @export
 cabat <- function(
   num_items = 25L,
@@ -7,16 +49,25 @@ cabat <- function(
   item_bank_audio = "http://media.gold-msi.org/test_materials/BAT/v1/audio",
   practice_items = "http://media.gold-msi.org/test_materials/BAT/v1/practice-items",
   next_item.criterion = "bOpt",
-  next_item.estimator = "WL",
+  next_item.estimator = "BM",
   next_item.prior_dist = "norm",
   next_item.prior_par = c(0, 1),
   final_ability.estimator = "WL",
   constrain_answers = FALSE,
+  fix_first_item = FALSE,
   dict = cabat::cabat_dict
 ) {
-  stopifnot(is.scalar.character(label), is.scalar.numeric(num_items),
-            is.scalar.logical(take_training), is.scalar.character(item_bank_audio),
-            is.scalar.character(practice_items))
+  stopifnot(is.scalar.integerlike(num_items),
+            is.scalar.logical(take_training)
+            is.scalar.character(label),
+            is.scalar.character(item_bank_audio),
+            is.scalar.character(practice_items),
+            is.scalar.character(next_item.criterion),
+            is.scalar.character(next_item.estimator),
+            is.scalar.character(next_item.prior_dist),
+            is.numeric(next_item.prior_par),
+            length(next_item.prior_par) == 2L,
+            is.scalar.character(final_ability.estimator))
   item_bank_audio <- gsub("/$", "", item_bank_audio)
   practice_items <- gsub("/$", "", practice_items)
   arg <- as.list(environment())
--- R/options.R
+++ R/options.R
@@ -5,7 +5,8 @@ get_options <- function(item_bank, arg) {
     next_item.prior_dist = arg$next_item.prior_dist,
     next_item.prior_par = arg$next_item.prior_par,
     final_ability.estimator = arg$final_ability.estimator,
+    constrain_answers = arg$constrain_answers,
     avoid_duplicates = "track_name",
-    eligible_first_items = c(23L, 24L)
+    eligible_first_items = if (arg$fix_first_item) c(23L, 24L) else NULL
   )
 }
